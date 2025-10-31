01d6d72af2d0e4272d5a77138503c309b08485ed
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-31 13:48:05 -0400

    Created draft of BattleshipButton

---
+++ src/BattleshipButton.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+
+public class BattleshipButton extends JButton
+{
+    private int row;
+    private int col;
+    private boolean blank;
+    private boolean miss;
+    private boolean hit;
+    private ImageIcon blankIcon;
+    private ImageIcon missIcon;
+    private ImageIcon hitIcon;
+
+    public BattleshipButton(int row, int col)
+    {
+        super();
+        this.row = row;
+        this.col = col;
+        blankIcon = new ImageIcon("src/wave.png");
+        missIcon = new ImageIcon("src/miss.png");
+        hitIcon = new ImageIcon("src/hit.png");
+        isBlank();
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean getBlank() {
+        return blank;
+    }
+
+    public boolean getMiss() {
+        return miss;
+    }
+
+    public boolean getHit() {
+        return hit;
+    }
+
+    public void isBlank() {
+        blank = true;
+        miss = false;
+        hit = false;
+        setIcon(blankIcon);
+    }
+
+    public void isMiss() {
+        miss = true;
+        blank = false;
+        hit = false;
+        setIcon(missIcon);
+    }
+
+    public void isHit() {
+        hit = true;
+        miss = false;
+        blank = false;
+        setIcon(hitIcon);
+    }
+}
