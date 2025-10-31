0fea696e51138a2280808f3ef85fe72aa22953de
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-31 13:59:45 -0400

    Created a draft of the BoardPnl, including an actionlistener draft

--- src/BoardPnl.java
+++ src/BoardPnl.java
@@ -1,10 +1,65 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class BoardPnl extends JPanel
 {
     BattleshipButton[][] boardGUI;
-    //need a listener
-    //need a game instance
+    BattleshipButtonListener listener;
+    BattleshipGame game;
 
+    public BoardPnl(BattleshipGame game)
+    {
+        this.game = game;
+        setLayout(new GridLayout(10, 10));
 
+        boardGUI = new BattleshipButton[10][10];
+
+        listener = new BattleshipButtonListener();
+
+        for(int row = 0; row < 10; row++)
+        {
+            for(int col = 0; col < 10; col++)
+            {
+                boardGUI[row][col] = new BattleshipButton(row, col);
+                boardGUI[row][col].addActionListener(listener);
+                add(boardGUI[row][col]);
+            }
+        }
+    }
+
+    public BattleshipButton[][] getBoardGUI() {
+        return boardGUI;
+    }
+
+    public BattleshipGame getGame() {
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BoardPnl boardPnl = (BoardPnl) o;
+        return Objects.deepEquals(boardGUI, boardPnl.boardGUI) && Objects.equals(listener, boardPnl.listener) && Objects.equals(game, boardPnl.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.deepHashCode(boardGUI), listener, game);
+    }
+
+    private class BattleshipButtonListener implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent ae)
+        {
+            if(ae.getSource() instanceof BattleshipButton selectedButton)
+            {
+                //game.buttonAction
+            }
+        }
+    }
 }
