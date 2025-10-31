17ec7d6d9ce462d0ffc165869c2f0852e38f3876
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-31 14:22:46 -0400

    Created a draft of ControlPnl

---
+++ src/ControlPnl.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Objects;
+
+public class ControlPnl extends JPanel
+{
+    JButton playAgainBtn;
+    JButton quitBtn;
+    BattleshipGame game;
+
+    public ControlPnl(BattleshipGame game)
+    {
+        this.game = game;
+        setLayout(new GridLayout(1, 2));
+
+        playAgainBtn = new JButton("Play Again");
+        quitBtn = new JButton("Quit");
+
+        add(playAgainBtn);
+
+        playAgainBtn.addActionListener((ActionEvent ae) -> {
+            int selection = JOptionPane.showConfirmDialog(null, "Are you sure you want to start a new game?", "Play Again", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+            if(selection == JOptionPane.YES_OPTION)
+            {
+                JOptionPane.showMessageDialog(null, "Starting a new game...");
+                game.clearBoard();
+            } else {
+                JOptionPane.showMessageDialog(null, "Your existing game will remain open.");
+            }
+        });
+
+        add(quitBtn);
+        quitBtn.addActionListener((ActionEvent ae) -> {
+            //This int tracks whether the user confirmed or denied they wanted to quit the program
+            int selection = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", "Quit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+            //This algorithm determines whether to quit the program based on the user's input
+            if(selection == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Quitting the program...");
+                System.exit(0);
+            } else
+            {
+                JOptionPane.showMessageDialog(null, "The program will remain open.");
+            }
+        });
+    }
+
+    public JButton getPlayAgainBtn() {
+        return playAgainBtn;
+    }
+
+    public JButton getQuitBtn() {
+        return quitBtn;
+    }
+
+    public BattleshipGame getGame() {
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ControlPnl that = (ControlPnl) o;
+        return Objects.equals(playAgainBtn, that.playAgainBtn) && Objects.equals(quitBtn, that.quitBtn) && Objects.equals(game, that.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playAgainBtn, quitBtn, game);
+    }
+}
