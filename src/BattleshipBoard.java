4cd43ab414b6fcaa7542e6eb6aa76791731bc6ba
Zoe Aspenns <aspennza@mail.uc.edu>
2025-11-01 19:37:29 -0400

    Finished writing the logic for checking if a ship has been sunk

--- src/BattleshipBoard.java
+++ src/BattleshipBoard.java
@@ -22,7 +22,7 @@ public class BattleshipBoard
         {
             for(int col = 0; col < COL; col++)
             {
-                //0 will represent blank cell, 1 will represent miss, 6 will represent hit
+                //0 will represent blank cell, 1 will represent miss, 6,7,8, and 9 will represent a hit for each ship
                 board[row][col] = 0;
             }
         }
@@ -35,12 +35,76 @@ public class BattleshipBoard
 
     public boolean isValidMove(int row, int col)
     {
-        return board[row][col] == 0;
+        return board[row][col] != 1 && board[row][col] != 6;
     }
 
-    public void setMiss(int row, int col)
+    public boolean calculateMove(int row, int col)
     {
+        if(board[row][col] == 0)
+        {
+            board[row][col] = 1;
+            //false represents a miss
+            return false;
+        } else {
+            board[row][col] = 6;
+            //true represents a hit
+            return true;
+        }
+    }
 
+    public boolean checkIfSunk(Ship ship, int row, int col)
+    {
+        int shipNum = checkShipSinkNum(ship);
+
+        if(board[row][col] != shipNum)
+        {
+            return false;
+        }
+        int consecCounter = 1;
+
+        for(int consecCol = col - 1; consecCol >= 0 && board[row][consecCol] == shipNum; consecCol--)
+        {
+            consecCounter++;
+        }
+
+        for(int consecCol = col + 1; consecCol < board[row].length && board[row][consecCol] == shipNum; consecCol++)
+        {
+            consecCounter++;
+        }
+
+        if(consecCounter == ship.getLength())
+        {
+            return true;
+        }
+
+        consecCounter = 1;
+
+        for(int consecRow = row - 1; consecRow >= 0 && board[consecRow][col] == shipNum; consecRow--) {
+            consecCounter++;
+        }
+
+        for(int consecRow = row + 1; consecRow < board.length && board[consecRow][col] == shipNum; consecRow++) {
+            consecCounter++;
+        }
+
+        if(consecCounter == ship.getLength()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int checkShipSinkNum(Ship ship)
+    {
+        if(ship.getLength() == 5) {
+            return 9;
+        } else if(ship.getLength() == 4) {
+            return 8;
+        } else if(ship.getLength() == 3) {
+            return 7;
+        } else {
+            return 6;
+        }
     }
 
     @Override
--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 
 public class BattleshipGame
 {
+    private static final int TURNS_TO_SINK = 2;
     private JFrame frame;
     private TitlePnl titlePnl;
     private BoardPnl boardPnl;
@@ -12,11 +13,22 @@ public class BattleshipGame
     private ControlPnl controlPnl;
     private BattleshipBoard board;
     private boolean playing;
+    Ship ship1 = new Ship(5);
+    Ship ship2 = new Ship(4);
+    Ship ship3 = new Ship(3);
+    Ship ship4 = new Ship(3);
+    Ship ship5 = new Ship(2);
 
     public void buttonAction(int row, int col)
     {
         if(playing) {
-            if(boardPnl.i
+            if(board.isValidMove(row, col))
+            {
+                board.setIndex(row, col, );
+            } else
+            {
+                JOptionPane.showMessageDialog(frame, "This space is already occupied. Please select a different space.");
+            }
         } else
         {
             JOptionPane.showMessageDialog(frame, "The game is over. Select Play Again to replay!");
