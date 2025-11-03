063244fc868bae095b9e77a8285a3f3c80c28dc9
Zoe Aspenns <aspennza@mail.uc.edu>
2025-11-03 18:39:54 -0500

    Completed Javadoc through BattleshipButton

--- src/BattleshipBoard.java
+++ src/BattleshipBoard.java
@@ -1,12 +1,24 @@
 import java.util.Arrays;
 import java.util.Objects;
 
+/**
+ * Allows the creation of objects representing the logic of a Battleship board.
+ * Contains rows, columns, and a final array of doubles representing the board indices.
+ * Also includes methods for acting on, updating, and comparing BattleshipBoard objects.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class BattleshipBoard
 {
+    //This int permanently stores the number of rows on the Battleship board
     private static final int ROW = 10;
+
+    //This int permanently stores the number of columns on the Battleship board
     private static final int COL = 10;
+
+    //This double array represents the 10 by 10 board used for Battleship
     private double[][] board;
 
+    //This constructor establishes the board and fills it with empty spaces
     public BattleshipBoard()
     {
         board = new double[ROW][COL];
@@ -17,6 +29,9 @@ public class BattleshipBoard
         return board;
     }
 
+    /**
+     * Sets all indices in the board to 0, representing a blank (non-miss and non-hit) space in battleship
+     */
     public void clearBoard()
     {
         for(int row = 0; row < ROW; row++)
@@ -29,16 +44,34 @@ public class BattleshipBoard
         }
     }
 
+    /**
+     * Sets the provided row and column index on the board to the provided ship ID number, thus placing part of the ship on that space
+     * @param row the selected row location
+     * @param col the selected column location
+     * @param shipNum the ship's ID number
+     */
     public void setIndex(int row, int col, double shipNum)
     {
         board[row][col] = shipNum;
     }
 
+    /**
+     * Tests if a player's selected move location is valid by determining whether the slot on the board is blank, a miss, or a hit
+     * @param row the player's selected row for their move
+     * @param col the player's selected column for their move
+     * @return a boolean representing whether the player's move choice is valid
+     */
     public boolean isValidMove(int row, int col)
     {
         return board[row][col] != 1 && board[row][col] != 6 && board[row][col] != 7 && board[row][col] != 8 && board[row][col] != 9;
     }
 
+    /**
+     * Determines what value to set a given index to depending on its current value (blank or a specific ship's value)
+     * @param row the player's chosen row location for their move
+     * @param col the player's chosen column location for their move
+     * @return a boolean representing whether the move resulted in a miss or a hit
+     */
     public boolean calculateMove(int row, int col)
     {
         if(board[row][col] == 0)
@@ -65,6 +98,14 @@ public class BattleshipBoard
         }
     }
 
+    /**
+     * Checks if a specific ship has been sunk by checking up, down, left, and right for its assigned "sunk" value; if all the tiles in the ship are sunk, it returns true
+     * @param shipLength the length of the ship being checked
+     * @param shipHitNum the value representing a "sunk" index for that specific ship
+     * @param row the row location of the player's move
+     * @param col the column location of the player's move
+     * @return a boolean representing whether the ship was sunk
+     */
     public boolean checkIfSunk(double shipLength, int shipHitNum, int row, int col)
     {
             if (board[row][col] != shipHitNum) {
@@ -72,10 +113,12 @@ public class BattleshipBoard
             }
             int consecCounter = 1;
 
+            //checking to the left
             for (int consecCol = col - 1; consecCol >= 0 && board[row][consecCol] == shipHitNum && consecCounter < shipLength; consecCol--) {
                 consecCounter++;
             }
 
+            //checking to the right
             for (int consecCol = col + 1; consecCol < board[row].length && board[row][consecCol] == shipHitNum && consecCounter < shipLength; consecCol++) {
                 consecCounter++;
             }
@@ -86,10 +129,12 @@ public class BattleshipBoard
 
             consecCounter = 1;
 
+            //checking to the north
             for (int consecRow = row - 1; consecRow >= 0 && board[consecRow][col] == shipHitNum && consecCounter < shipLength; consecRow--) {
                 consecCounter++;
             }
 
+            //checking to the south
             for (int consecRow = row + 1; consecRow < board.length && board[consecRow][col] == shipHitNum && consecCounter < shipLength; consecRow++) {
                 consecCounter++;
             }
--- src/BattleshipButton.java
+++ src/BattleshipButton.java
@@ -1,17 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Allows the creation of JButtons representing a single visible index on a
+ * Battleship board GUI. Stores its own row, column, and whether it is
+ * a blank tile, a missed tile, or a hit tile. Also includes methods
+ * for acting on and updating BattleshipButton objects.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class BattleshipButton extends JButton
 {
+    //The row location of the button
     private int row;
+
+    //The column location of the button
     private int col;
+
+    //A boolean representing whether the tile is blank
     private boolean blank;
+
+    //A boolean representing whether the tile is missed
     private boolean miss;
+
+    //A boolean representing whether the tile is hit
     private boolean hit;
+
+    //An ImageIcon for a blank tile
     private ImageIcon blankIcon;
+
+    //An ImageIcon for a missed tile
     private ImageIcon missIcon;
+
+    //An ImageIcon for a hit tile
     private ImageIcon hitIcon;
 
+    //This constructor establishes the row, column, and icons for the button and sets it to blank automatically
     public BattleshipButton(int row, int col)
     {
         super();
@@ -55,6 +78,9 @@ public class BattleshipButton extends JButton
         return hitIcon;
     }
 
+    /**
+     * This method sets a given JButton to blank and changes its icon to the blankIcon
+     */
     public void isBlank() {
         blank = true;
         miss = false;
@@ -64,6 +90,9 @@ public class BattleshipButton extends JButton
         setBackground(new Color(116, 180, 214));
     }
 
+    /**
+     * This method sets a given JButton to a miss and changes its icon to the missIcon; also disables it from being clicked
+     */
     public void isMiss() {
         miss = true;
         blank = false;
@@ -74,6 +103,9 @@ public class BattleshipButton extends JButton
         setBackground(new Color(246, 225, 95));
     }
 
+    /**
+     * This method sets a given JButton to a hit and changes its icon to the hitIcon; also disables it from being clicked
+     */
     public void isHit() {
         hit = true;
         miss = false;
